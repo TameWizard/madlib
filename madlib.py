ac4a266ee0595b60033619429b0131811131780f
unknown <admin2@OK2.proflingva.net>
2020-01-10 17:31:12 +0300

    Added a couple of comments

--- madlib.py
+++ madlib.py
@@ -1,11 +1,13 @@
 import nltk
 import random as rd
 
+
 NN = []
 fNN = open('dicNN.txt', 'r')
 for i in fNN:
     NN.append(i.replace('\n', ''))
 fNN.close()
+
 
 VB = []
 fVB = open('dicVB.txt', 'r')
@@ -13,16 +15,19 @@ for i in fVB:
     VB.append(i.replace('\n', ''))
 fVB.close()
 
+
 JJ = []
 fJJ = open('dicJJ.txt', 'r')
 for i in fJJ:
     JJ.append(i.replace('\n', ''))
 fJJ.close()
 
+
 def ser(tag):
     num = rd.randrange(0, len(tag))
     res = tag[num]
     return(res)
+            
     
 def make_new_items(tok_text):
     new_items = []
@@ -44,15 +49,37 @@ def make_new_items(tok_text):
                 new_items.append(ser(JJ))
         else:
             new_items.append(i)
-    return new_items
+    return(new_items)
+
 
 with open(r'full_text.txt', 'r') as text:
     tok_text = nltk.word_tokenize(text.read())  # tokenization
-    tags = nltk.pos_tag(tok_text)  # tagging
-    lVB = [i[0] for i in tags if 'VBD' in i] # find verbs
-    lNN = [i[0] for i in tags if 'NN' in i] # find nouns
-    lJJ = [i[0] for i in tags if 'JJ' in i] # find adj
-    almost_ready = make_new_items(tok_text)  # replace words
+    clean_tok_text = [] # clean tags (does not work as a separate function)
+    for i in tok_text:
+        if i == 'вЂњ': # change most common type of coding error
+            i = '"'
+            clean_tok_text.append(i)
+        elif i == "вЂќ":
+            i = "'"
+            clean_tok_text.append(i)
+        elif i == "вЂ™":
+            i = "'"
+            clean_tok_text.append(i)
+        elif i == "вЂ":
+            i = "-"
+            clean_tok_text.append(i)
+        else:
+            i = i.replace("вЂќ", "'")
+            i = i.replace("вЂ™", "'")
+            i = i.replace("вЂњ", '"')
+            i = i.replace("Ђ™", "'")
+            clean_tok_text.append(i)
+    tags = nltk.pos_tag(tok_text)  # tagging, no need to clean here
+    lVB = [i[0] for i in tags if i[1] == 'VBD'] # find verbs
+    lNN = [i[0] for i in tags if i[1] == 'NN'] # find nouns
+    lJJ = [i[0] for i in tags if i[1] == 'JJ'] # find adj
+    almost_ready = make_new_items(clean_tok_text)  # replace words
+
 
 def make_text(text):  # unite the string 
     stop = ['!', '.', ',', '?', ':', ';', "n't", "'m", "'s", "'", "'re"]
